2debf67e0d8ce6b9b28b5aaf8c80145d425af657
Jess Balint <jbalint@gmail.com>
2013-10-19 11:18:21 -0500

    add some prototype code

---
+++ prototype-code/dzen2_IDO.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/zsh
+
+cat > /tmp/dzen2_proto_values <<EOF
+put sample/test data here
+EOF
+
+rm -f /tmp/dzen2_proto_pipe && mkfifo /tmp/dzen2_proto_pipe
+dzen2 -xs 1 -e '' -l 22 -fn "-*-inconsolata-*-*-*-*-*-*-*-*-*-*-*-*" -m < /tmp/dzen2_proto_pipe &
+exec 3> /tmp/dzen2_proto_pipe
+q=""
+echo "^uncollapse()" >&3
+#set -x
+./a.out 2>&1 | while read i ; do
+	if [ $i = "C-s" ] ; then
+		selected=$(expr $selected + 1)
+	elif [ $i = "C-r" ] ; then
+		selected=$(expr $selected - 1)
+		if [ $selected = 0 ] ; then
+			selected=1
+		fi
+	else
+		if [ $i = "BackSpace" ] ; then
+			q=${q%?}
+		else
+			q="$q$i"
+		fi
+		selected=1
+		echo "^tw()^fg(#cb4b16)$q" >&3
+		rows=()
+		grep -i "$q" /tmp/dzen2_proto_values | head -n 20 | sed "s/\($q\)/^fg(#859900)\1^fg()/ig" | while read j ; do
+			#rows+=($j)
+			rows[$(($#rows+1))]=$j
+		done
+	fi
+	echo "^cs()" >&3
+	OUTPUT=""
+	for n in $(seq 1 ${#rows[@]}) ; do
+		x=""
+		if [ $n = $selected ] ; then
+			x="^bg(#586e75)"
+		fi
+		OUTPUT+="$x${rows[$n]}\n"
+	done
+	echo "$OUTPUT" >&3
+done
---
+++ prototype-code/grabkey.c
@@ -0,0 +1,78 @@
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *modkey_names[] = {
+	"Shift",
+	"",
+	"Control",
+	"Mod1",
+	"Mod2",
+	"Mod3",
+	"Mod4",
+	"Mod5",
+	NULL
+};
+
+int main(int argc, char **argv)
+{
+  Display *d;
+  XEvent ev;
+  unsigned int mods;
+  KeySym ks;
+  char buf[20];
+  const char *kss;
+  char *bufptr;
+  d = XOpenDisplay(NULL);
+  XGrabKeyboard(d, DefaultRootWindow(d), False, GrabModeAsync, GrabModeAsync, CurrentTime);
+  while (1)
+  {
+	assert(!XWindowEvent(d, DefaultRootWindow(d), KeyPressMask, &ev));
+	XkbLookupKeySym(d, ev.xkey.keycode, ev.xkey.state, &mods, &ks);
+	/* ignore the modifier keypresses */
+	switch (ks) /* from keysymdef.h */
+	{
+case XK_Shift_L   :  /* Left shift */
+case XK_Shift_R   :  /* Right shift */
+case XK_Control_L :  /* Left control */
+case XK_Control_R :  /* Right control */
+case XK_Caps_Lock :  /* Caps lock */
+case XK_Shift_Lock:  /* Shift lock */
+case XK_Meta_L    :  /* Left meta */
+case XK_Meta_R    :  /* Right meta */
+case XK_Alt_L     :  /* Left alt */
+case XK_Alt_R     :  /* Right alt */
+case XK_Super_L   :  /* Left super */
+case XK_Super_R   :  /* Right super */
+case XK_Hyper_L   :  /* Left hyper */
+case XK_Hyper_R   :  /* Right hyper */
+ continue;
+	}
+	kss = XKeysymToString(ks);
+	
+	bufptr = buf;
+	if (ev.xkey.state & ShiftMask) { }
+	if (ev.xkey.state & ControlMask)
+	  bufptr += sprintf(bufptr, "C-");
+	if (ev.xkey.state & Mod1Mask)
+	  bufptr += sprintf(bufptr, "M-");
+	if (ev.xkey.state & Mod2Mask) { }
+	if (ev.xkey.state & Mod3Mask) { }
+	if (ev.xkey.state & Mod4Mask)
+	  bufptr += sprintf(bufptr, "S-"); // S = super
+	bufptr += sprintf(bufptr, kss);
+	fprintf(stderr, "%s\n", buf);
+	//printf("%s\n", buf);
+	/* { */
+	/*   Window cur; */
+	/*   int revert_to_return; */
+	/*   XGetInputFocus(d, &cur, &revert_to_return); */
+	/*   XSendEvent(d, cur, False, KeyPressMask, &ev); */
+	/* } */
+  }
+  XCloseDisplay(d);
+}
