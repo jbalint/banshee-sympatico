26af6a3dfffc1941a7ff22d1fb9532bbcb326fd3
Jess Balint <jbalint@gmail.com>
2014-06-04 12:59:42 -0500

    notes on LUA environment

---
+++ lua/bs_lua_env.sh
@@ -0,0 +1,7 @@
+# this must be SOURCE'd, not EXECUTED
+
+# BS_HOME should be set globally
+#export BS_HOME=/somewhere
+
+export LUA_PATH="$BS_HOME/lua/share/lua/5.2/?.lua;$BS_HOME/lua/share/lua/5.2/?/init.lua;;"
+export LUA_CPATH="$BS_HOME/lua/lib/lua/5.2/?.so;;"
