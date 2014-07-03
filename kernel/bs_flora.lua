7079f528d9de1911f514eafba8c65262c9354a79
Jess Balint <jbalint@gmail.com>
2014-07-03 14:34:20 -0500

    add classes and instances search to bs_flora

--- kernel/bs_flora.lua
+++ kernel/bs_flora.lua
@@ -1,8 +1,28 @@
 local flora = {}
 
+local sep = "\x03"
+
 -- Flora's internal prefix coding for IRIs
 local function _flora_iri_to_string(iri)
-   return iri:gsub("^i\x08", "")
+   -- () -> return only the first value
+   return (iri:gsub("^i\x08", ""))
+end
+
+local
+function _parse_xsb_output(str, fields)
+   local i = 1
+   local j = str:find(sep)
+
+   local vals = {}
+   for num = 1, fields do
+	  table.insert(vals, str:sub(i, j - 1))
+	  i = j + 1
+	  j = str:find(sep, i)
+	  if not j and num < fields then
+		 error("Not enough values available for parsing")
+	  end
+   end
+   return table.unpack(vals)
 end
 
 function flora.getprefixes()
@@ -19,12 +39,10 @@ end
 
 function flora.getproperties(objname)
    local query = string.format("%s[?X -> ?Y].", objname)
-   local qres = xsb_query(string.format("flora_query(\"%s\", [\"?X\"=X,\"?Y\"=Y],_,_,_).", query), "\x03")
+   local qres = xsb_query(string.format("flora_query(\"%s\", [\"?X\"=X,\"?Y\"=Y],_,_,_).", query), sep)
    local propvals = {}
    for idx, str in ipairs(qres) do
-	  local i = str:find("\x03")
-	  local prop = str:sub(1, i - 1)
-	  local val = str:sub(i + 1, -1 + str:find("\x03", i + 1))
+	  local prop, val = _parse_xsb_output(str, 2)
 	  if propvals[prop] then
 		 -- handle sets dynamically
 		 if type(propvals[prop]) ~= "table" then
@@ -39,6 +57,43 @@ function flora.getproperties(objname)
 	  end
    end
    return propvals
+end
+
+function flora.classes(objname)
+   -- we only consider IRIs as classes, not other values
+   local query = string.format("%s:?X^^\\iri.", objname)
+   local qres = xsb_query(string.format("flora_query(\"%s\", [\"?X\"=X],_,_,_).", query), sep)
+   local classes = {}
+   local _dupcheck = {}
+   for idx, str in ipairs(qres) do
+	  if not _dupcheck[str] then
+		 table.insert(classes, _flora_iri_to_string(_parse_xsb_output(str, 1)))
+		 _dupcheck[str] = 1
+	  end
+   end
+   return classes
+end
+
+function flora.instances(classname)
+   -- TODO generalize with classes() method
+   local query = string.format("?X^^\\iri:%s.", classname)
+   local qres = xsb_query(string.format("flora_query(\"%s\", [\"?X\"=X],_,_,_).", query), sep)
+   local instances = {}
+   -- we need to remove identical answers from the Flora query
+   -- c.f. flranswer.P, flora_write_matches,
+   -- flora_skip_identical_answers
+   local _dupcheck = {}
+   for idx, str in ipairs(qres) do
+	  if not _dupcheck[str] then
+		 table.insert(instances, _flora_iri_to_string(_parse_xsb_output(str, 1)))
+		 _dupcheck[str] = 1
+	  end
+   end
+   return instances
+end
+
+function flora.shell()
+   xsb_command("flora_shell.")
 end
 
 return flora
