97603cc232beecf3f31c591672cb6c6df9cabbb9
Jess Balint <jbalint@gmail.com>
2014-07-03 14:36:07 -0500

    add dates to boxinglove

--- kernel/bs_boxinglove.lua
+++ kernel/bs_boxinglove.lua
@@ -8,6 +8,8 @@ local flora = require('bs_flora')
 
 local _dump = require('pl.pretty').dump
 
+local Date = require('pl.Date')
+
 -- Wrapper for custom index metamethod. We pass all requests for keys
 -- that don't start with "_" to the custom index method. If it does
 -- begin with underscore, it implies that it's an internal
@@ -69,7 +71,6 @@ function blreference:_index(k)
 end
 
 function blreference:__call()
-   print(1)
    return _blnamespace_loadobject(self._boxinglove_ns, self._boxinglove_name)
 end
 
@@ -86,6 +87,7 @@ function _blparseuri(uri)
    -- tested with
    --uri = "http://banshee-sympatico/project#MysqlConnectorJ"
    --uri = "http://purl.org/dc/terms/title"
+   assert(uri:find("http://") == 1)
    local hash = uri:find("#")
    if hash then
 	  local base = uri:sub(1, hash)
@@ -96,6 +98,8 @@ function _blparseuri(uri)
    return base, name
 end
 
+-- TODO this could probably be properly in the Flora module as long as
+-- marshalling/unmarshalling is consistent
 function _blparse(value)
    -- TODO audit / finish this
    -- flora type encoding
@@ -103,9 +107,15 @@ function _blparse(value)
    if flenc_type then
 	  flenc_type = flenc_type:sub(1, 1)
 	  if flenc_type == "i" then
-		 local base, name = _blparseuri(value:sub(3))
-		 local ns = boxinglove.ns._byuri[base]
-		 return _blreference_new(ns, name)
+		 value = value:sub(3)
+		 if value:find("mailto:") == 1 then
+			-- TODO find something to do with non object resources
+			print("WARN: " .. "UNHANDLED EMAIL ADDRESS: " .. tostring(value))
+		 else
+			local base, name = _blparseuri(value)
+			local ns = boxinglove.ns._byuri[base]
+			return _blreference_new(ns, name)
+		 end
 	  elseif flenc_type == "s" then
 		 return value:sub(3)
 	  else
@@ -118,7 +128,30 @@ function _blparse(value)
 	  if flenc_type == "long" then
 		 local str = value:gsub("'\\datatype'%('\\long'%((%d+)%),'\\long'%)", "%1")
 		 return tonumber(str)
+	  elseif flenc_type == "date" then
+		 local capture = value:gmatch("%d+")
+		 capture() -- skip first one - date "sign"
+		 local year = capture()
+		 local month = capture()
+		 local day = capture()
+		 return Date{year = year, month = month, day = day}
+	  elseif flenc_type == "dateTime" then
+		 local capture = value:gmatch("%d+")
+		 capture()
+		 local year = capture()
+		 local month = capture()
+		 local day = capture()
+		 local hour = capture()
+		 local minute = capture()
+		 local seconds = capture()
+		 return Date{year = year,
+					 month = month,
+					 day = day,
+					 hour = hour,
+					 min = min,
+					 sec = sec}
 	  else
+		 require("pl.pretty").dump(value)
 		 error("Unknown Flora encoding for type: " .. tostring(flenc_type))
 	  end
    end
@@ -131,7 +164,7 @@ end
 function _blinstance_populate(self, data)
    for prop, val in pairs(data) do
 	  --prop = _blparse(prop)
-	  -- TODO not clean
+	  -- TODO not clean way to deal with prop IRI
 	  prop = prop:sub(3)
 	  if type(val) == "table" then
 		 local vals = {}
@@ -193,7 +226,13 @@ function _blnamespace_loadobject(ns, name)
    -- TODO optimization: return instance if name begins with a capital
    -- letter, reference otherwise. this avoids populating property
    -- objects that will infrequently be used
-   local data = flora.getproperties(string.format("%s#%s", ns._boxinglove_prefix, name))
+   local fullname = string.format("%s#%s", ns._boxinglove_prefix, name)
+   local data = flora.getproperties(fullname)
+   if next(data) == nil then
+	  -- TODO this *should* double-check because the object might be
+	  -- "empty" but Flora object existence is wonky
+	  error("Object doesn't exist: " .. fullname)
+   end
    return _blinstance_new(ns, name, data)
 end
 
@@ -216,7 +255,7 @@ function boxinglove.init()
 	  -- TODO maybe this should happen external this module? (eases
 	  -- testing, better design, etc)
 	  -- we pollute the global environment with namespaces
-	  --_G[prefix] = ns
+	  _G[prefix] = ns
    end
 end
 
