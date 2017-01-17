f468370bf63e3f7689bc7fdfc35a24ba97e29a37
Jess Balint <jbalint@gmail.com>
2017-01-16 20:49:29 -0600

    change script to use DB instead of filesystem [BS-30]

--- percy/percy-bookstore.sh
+++ percy/percy-bookstore.sh
@@ -1,7 +1,62 @@
 #!/bin/bash
-# Script to open bookstore files. Hopefully this script is replaced
-# soon by a Stardog-backed version with good schema
+# Script to query the bookstore entries for Helm completion source
 
-BOOKSTORE_ROOT=/home/jbalint/Dropbox/bookstore
+# TODO: error handling is bad
 
-echo '`(' ; find ~/Dropbox/bookstore/ -type f | perl -lpe's#/home/jbalint/Dropbox/bookstore/(.*)#("BOOK: $1" . "https://localhost/bookstore/$1")#' ; echo ')'
+#set -x
+QUERY=$(cat<<EOF
+select *
+from <http://banshee-sympatico/bookstore>
+where {
+  ?b a bslib:Book ; nie:isStoredAs [
+  nfo:fileName ?fileName ;
+  nie:url ?url ;
+  nfo:fileCreated ?created
+  ]
+} order by desc(?created)
+EOF
+)
+#echo "$QUERY"
+echo '`(' ; \
+    curl -u admin:admin  -H "Accept: application/sparql-results+json" -G https://localhost/stardog/bs/query \
+         --data-urlencode query="$QUERY" 2> /dev/null \
+        | jq -r '.results.bindings[] | @text "(\"BOOK: \(.fileName.value)\" . \"\(.url.value)\")"' \
+    ; echo ")"
+
+    
+# SEXP output:
+#("BOOK: equipment_docs/intel-q87-8-series-chipset-pch-datasheet.pdf" . "https://localhost/bookstore/equipment_docs/intel-q87-8-series-chipset-pch-datasheet.pdf")
+#("BOOK: equipment_docs/ThinkCentre-spec-v491-June2016.pdf" . "https://localhost/bookstore/equipment_docs/ThinkCentre-spec-v491-June2016.pdf")
+#("BOOK: equipment_docs/Intel_Linux_NVMe_Guide_330602-002.pdf" . "https://localhost/bookstore/equipment_docs/Intel_Linux_NVMe_Guide_330602-002.pdf")
+
+# Output is something like
+  #   {
+  # "head" : {
+  #   "vars" : [
+  #     "b",
+  #     "fileName",
+  #     "url",
+  #     "created"
+  #   ]
+  # },
+  # "results" : {
+  #   "bindings" : [
+  #     {
+  #       "b" : {
+  #         "type" : "bnode",
+  #         "value" : "node1b6knlfc7x528"
+  #       },
+  #       "fileName" : {
+  #         "type" : "literal",
+  #         "value" : "File Name.djvu"
+  #       },
+  #       "url" : {
+  #         "type" : "uri",
+  #         "value" : "https://localhost/bookstore/File%20Name.djvu"
+  #       },
+  #       "created" : {
+  #         "datatype" : "http://www.w3.org/2001/XMLSchema#dateTime",
+  #         "type" : "literal",
+  #         "value" : "2017-01-16T10:11:19-06:00"
+  #       }
+  #     },
