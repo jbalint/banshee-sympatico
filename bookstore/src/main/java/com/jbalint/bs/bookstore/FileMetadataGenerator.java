e3a94240379bc6c2fd175d6512eda005133f1fa6
Jess Balint <jbalint@gmail.com>
2017-01-24 16:53:50 -0600

    Clean up BS FileMetadataGenerator, fixed "relativePath" issue.
    [BS-29]

--- bookstore/src/main/java/com/jbalint/bs/bookstore/FileMetadataGenerator.java
+++ bookstore/src/main/java/com/jbalint/bs/bookstore/FileMetadataGenerator.java
@@ -3,24 +3,19 @@ package com.jbalint.bs.bookstore;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
-import java.util.Objects;
 
 import com.complexible.common.openrdf.util.ModelBuilder;
 import com.complexible.common.openrdf.util.ResourceBuilder;
 import com.complexible.common.rdf.model.StardogValueFactory;
 import com.complexible.stardog.api.Connection;
 import com.complexible.stardog.api.ConnectionConfiguration;
-import com.complexible.stardog.api.Getter;
 import com.jbalint.bs.ontology.BsLib;
 import com.jbalint.bs.ontology.Nepomuk;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.mutable.MutableInt;
 import org.openrdf.model.IRI;
 import org.openrdf.model.Model;
 
@@ -32,45 +27,55 @@ public class FileMetadataGenerator {
 
 	Connection conn;
 	Path root;
-	Getter get;
 	IRI context;
+
+	public FileMetadataGenerator(Connection conn, Path root, IRI context) {
+		this.conn = conn;
+		this.root = root;
+		this.context = context;
+	}
+
+	/**
+	 * Does an entry for the corresponding path exist in the DB?
+	 */
+	boolean existsInDb(Path p) {
+		return conn.get().context(context).predicate(Nepomuk.NIE.url).object(getIRI(p)).statements().findFirst().isPresent();
+	}
 
 	/**
 	 * Turn a Path into a model if it doesn't already exist in the DB and it's a regular file
 	 */
 	Model file(Path p) {
 		try {
-			if (Files.isRegularFile(p)) {
-				get.reset();
-				if (get.context(context).predicate(Nepomuk.NIE.url).object(getIRI(p)).statements().findFirst().isPresent()) {
-					// already exists
-					return null;
-				}
-				Files.getLastModifiedTime(p);
-				ResourceBuilder mainFile = new ModelBuilder()
-					                           .instance(Nepomuk.NFO.RemoteDataObject)
-					                           .addProperty(Nepomuk.NIE.url, getIRI(p))
-					                           .addProperty(BsLib.relativePath, p.relativize(root).toString())
-					                           .addProperty(Nepomuk.NIE.byteSize, Files.size(p))
-					                           .addProperty(Nepomuk.NIE.lastRefreshed, new Date())
-					                           .addProperty(Nepomuk.NFO.fileCreated, new Date(Files.getLastModifiedTime(p).toMillis()))
-					                           .addProperty(Nepomuk.NFO.fileName, p.getFileName().toString())
-					                           .addProperty(Nepomuk.NFO.hasHash, new ModelBuilder().instance(Nepomuk.NFO.FileHash)
-					                                                                               .addProperty(Nepomuk.NFO.hashAlgorithm, "MD5")
-					                                                                               .addProperty(Nepomuk.NFO.hashValue,
-					                                                                                            DigestUtils.md5Hex(Files.newInputStream(p))));
-				ResourceBuilder infoElement = new ModelBuilder()
-					                              .instance(Nepomuk.NIE.InformationElement)
-					                              .addProperty(StardogValueFactory.RDF.TYPE, BsLib.Book)
-					                              .addProperty(Nepomuk.NIE.isStoredAs, mainFile);
+			Files.getLastModifiedTime(p);
+			ResourceBuilder mainFile = new ModelBuilder()
+				                           .instance(Nepomuk.NFO.RemoteDataObject)
+				                           .addProperty(Nepomuk.NIE.url, getIRI(p))
+				                           .addProperty(BsLib.relativePath, root.relativize(p).toString())
+				                           .addProperty(Nepomuk.NIE.byteSize, Files.size(p))
+				                           .addProperty(Nepomuk.NIE.lastRefreshed, new Date())
+				                           .addProperty(Nepomuk.NFO.fileCreated, new Date(Files.getLastModifiedTime(p).toMillis()))
+				                           .addProperty(Nepomuk.NFO.fileName, p.getFileName().toString())
+				                           .addProperty(Nepomuk.NFO.hasHash, new ModelBuilder().instance(Nepomuk.NFO.FileHash)
+				                                                                               .addProperty(Nepomuk.NFO.hashAlgorithm, "MD5")
+				                                                                               .addProperty(Nepomuk.NFO.hashValue,
+				                                                                                            DigestUtils.md5Hex(Files.newInputStream(p))));
+			ResourceBuilder infoElement = new ModelBuilder()
+				                              .instance(Nepomuk.NIE.InformationElement)
+				                              .addProperty(StardogValueFactory.RDF.TYPE, BsLib.Book)
+				                              .addProperty(Nepomuk.NIE.isStoredAs, mainFile);
 
-				return infoElement.model();
-			}
+			return infoElement.model();
 		}
 		catch (IOException theE) {
-			theE.printStackTrace();
+			throw new RuntimeException(theE);
 		}
-		return null;
+	}
+
+	void addToDb(Model m) {
+		conn.begin();
+		conn.add().graph(m, context);
+		conn.commit();
 	}
 
 	private IRI getIRI(final Path p) {
@@ -92,19 +97,30 @@ public class FileMetadataGenerator {
 			System.err.println("Usage: FileMetadataGenerator <db-url> <fileroot> <graph-iri>");
 			System.exit(1);
 		}
-		FileMetadataGenerator fmg = new FileMetadataGenerator();
-		fmg.conn = ConnectionConfiguration.at(args[0]);
-		fmg.get = fmg.conn.get();
-		fmg.root = Paths.get(args[1]);
-		fmg.context = VF.createIRI(args[2]);
-		MutableInt booksAdded = new MutableInt(0);
-		Files.walk(fmg.root).map(fmg::file).filter(Objects::nonNull).forEach(m -> {
-			fmg.conn.begin();
-			fmg.conn.add().graph(m, fmg.context);
-			fmg.conn.commit();
-			booksAdded.increment();
-		});
+
+		FileMetadataGenerator fmg = new FileMetadataGenerator(ConnectionConfiguration.at(args[0]),
+		                                                      Paths.get(args[1]),
+		                                                      VF.createIRI(args[2]));
+
+		long booksAdded;
+		boolean TESTING = false;
+		if (TESTING) {
+			booksAdded = Files.walk(fmg.root)
+			                       .filter(Files::isRegularFile)
+			                       .limit(1)
+			                       .map(fmg::file)
+			                       .count();
+		} else {
+			booksAdded = Files.walk(fmg.root)
+			                       .filter(Files::isRegularFile)
+			                       .filter(f -> !fmg.existsInDb(f))
+			                       .map(fmg::file)
+			                       .peek(fmg::addToDb)
+			                       .count();
+		}
+
 		System.err.println("Books added: " + booksAdded);
+
 		//StatementSources.write(MemoryStatementSource.of(fmg.statements), RDFFormat.TURTLE, new FileOutputStream("/tmp/books.ttl"));
 	}
 }
