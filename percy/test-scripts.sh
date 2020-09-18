f4b2fcc1b67ec114f1c3343857d00ae262f933b3
Jess Balint <jbalint@gmail.com>
2020-09-18 12:24:51 -0500

    add insight-objects to test-scripts

--- percy/test-scripts.sh
+++ percy/test-scripts.sh
@@ -2,7 +2,7 @@
 
 # Diagnostic script to make sure percy sources are returning results
 
-for script in percy-bookmarks.sh percy-bookstore.sh percy-chromozol.sh percy-jira-issues.sh percy-wiki-pages.sh ; do
+for script in percy-bookmarks.sh percy-bookstore.sh percy-chromozol.sh percy-jira-issues.sh percy-jira-insight-objects.sh percy-wiki-pages.sh ; do
     echo -n "=== Executed $script. Results: "
     time $script | wc -l
 done
