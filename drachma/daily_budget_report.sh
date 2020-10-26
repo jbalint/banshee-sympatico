722241f30588b81c0f1d689f402f705fc55bed1d
Jess Balint <jbalint@gmail.com>
2020-10-26 11:56:03 -0500

    change venv to new version [SYS-165]

--- drachma/daily_budget_report.sh
+++ drachma/daily_budget_report.sh
@@ -1,1 +1,1 @@
-/home/jbalint/sw/banshee-sympatico/drachma/venv/bin/jupyter nbconvert --execute --to html daily_budget_report.ipynb
+/home/jbalint/sw/banshee-sympatico/drachma/venv2/bin/jupyter nbconvert --execute --to html daily_budget_report.ipynb
