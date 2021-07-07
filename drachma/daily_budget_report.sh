7fda491269cabe0456c6548f3e0e5be5302db73d
Jess Balint <jbalint@gmail.com>
2021-07-07 18:40:18 -0500

    fix reporting timing out [BS-91]

--- drachma/daily_budget_report.sh
+++ drachma/daily_budget_report.sh
@@ -1,1 +1,3 @@
-/home/jbalint/sw/banshee-sympatico/drachma/venv2/bin/jupyter nbconvert --execute --to html daily_budget_report.ipynb
+VENV=/home/jbalint/sw/banshee-sympatico/drachma/venv3
+. $VENV/bin/activate
+$VENV/bin/jupyter nbconvert --execute --ExecutePreprocessor.timeout=60 --to html daily_budget_report.ipynb
