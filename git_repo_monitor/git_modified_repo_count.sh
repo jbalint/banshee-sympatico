403be915f6e5c0aec628bcb25c243305612a8774
Jess Balint <jbalint@gmail.com>
2024-06-21 17:35:14 -0500

    git_repo_monitor output in Prometheus format

--- git_repo_monitor/git_modified_repo_count.sh
+++ git_repo_monitor/git_modified_repo_count.sh
@@ -1,6 +1,10 @@
 COUNT=$($BS_HOME/git_repo_monitor/target/release/git_repo_monitor print_json | wc -l)
 #COUNT=0
 
+mkdir -p /tmp/prom
+
+echo git_repos_uncommitted{host=\"$HOSTNAME\"} $COUNT > /tmp/prom/git_repo_monitor.prom
+
 if [[ $COUNT > 0 ]] ; then
     # Colors don't work with Polybar
     #echo -e "Git: \033[31m$COUNT\e[0m"
